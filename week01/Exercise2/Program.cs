d1938ca0c7c60443a97a8fe8f18cd82ccf1a934d
vamundse <vamundse@gmail.com>
2025-09-01 14:58:41 +0200

    Finished exercises 1, 2, and 3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage: ");
+        string percentageFromUser = Console.ReadLine();
+        string grade = "";
+        string pm = "";
+        int percentage = int.Parse(percentageFromUser);
+        int lastNumber = percentage % 10;
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+            if (lastNumber < 3)
+            {
+                pm = "-";
+            }
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+            if (lastNumber >= 7)
+            {
+                pm = "+";
+            }
+            else if (lastNumber < 3)
+            {
+                pm = "-";
+            }
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+            if (lastNumber >= 7)
+            {
+                pm = "+";
+            }
+            else if (lastNumber < 3)
+            {
+                pm = "-";
+            }
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+            if (lastNumber >= 7)
+            {
+                pm = "+";
+            }
+            else if (lastNumber < 3)
+            {
+                pm = "-";
+            }
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade: {grade}{pm}");
+
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string answer = "";
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int userGuess = 0;
+            int guesses = 0;
+
+            do
+            {
+                Console.Write("What is you guess? ");
+                string theUserGuess = Console.ReadLine();
+
+                userGuess = int.Parse(theUserGuess);
+                guesses++;
+
+                if (magicNumber > userGuess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < userGuess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guesses} guesses!");
+                }
+
+            } while (magicNumber != userGuess);
+
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            answer = Console.ReadLine();
+
+        } while (answer.ToLower() == "yes");
+
+        
+
     }
+
+    
 }
