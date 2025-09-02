b10c5a3c047850877c741d756b428499c21e2914
vamundse <vamundse@gmail.com>
2025-09-02 10:21:38 +0200

    Finished exercises 4-5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,95 @@
 using System;
+using System.Globalization;
+using System.Transactions;
+using System.Collections.Generic;
+
+class GFG : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        if (x == 0 || y == 0)
+        {
+            return 0;
+        }
+        
+        // CompareTo() method
+        return x.CompareTo(y);
+        
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        int sum = 0;
+        int rounds = 0;
+        int userNumber = 1;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string firstUserNumber = Console.ReadLine();
+            userNumber = int.Parse(firstUserNumber);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+            
+            rounds += 1;
+            sum += userNumber;
+        }
+
+        rounds--;
+        float average = (float)sum / rounds;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"Number of rounds: {rounds}");
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNumber = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+        }
+
+        int smallestNumber = 999999999;
+
+        foreach (int number in numbers)
+        {
+            if (number < smallestNumber && number > 0)
+            {
+                smallestNumber = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest postive number is: {smallestNumber}");
+
+        // "gg" is the object oif class GFG
+        GFG gg = new GFG();
+        
+        Console.WriteLine("The sorted list:");
+        
+        // use of List<T>.Sort(IComparer<T>) 
+        // method. The comparer is "gg"
+        numbers.Sort(gg);
+        
+        foreach( int g in numbers )
+        {
+            
+            // Display sorted list
+            Console.WriteLine(g);
+            
+        }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayResult();
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromtUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter you favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumber()
+    {
+        int num = PromptUserNumber();
+        int squared = num * num;
+        return squared;
+    }
+
+    static void DisplayResult()
+    {
+        DisplayWelcome();
+        string name = PromtUserName();
+        int squared = SquareNumber();
+        Console.WriteLine($"{name}, the square of your number is {squared}.");
     }
 }
