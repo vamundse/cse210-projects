b36fb85b35305e717693f797ec3d597c1df2fe7b
vamundse <vamundse@gmail.com>
2025-09-16 13:10:30 +0200

    Finished the fractions activity

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,63 @@
+using System.Data;
+using System.Diagnostics;
+
+public class Fractions
+{
+    private int _top;
+    private int _bottom;
+
+    public void Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public void Fraction(int wholeNumber)
+    {
+        SetTop(wholeNumber);
+        _bottom = 1;
+    }
+
+    public void Fraction(int top, int bottom)
+    {
+        SetTop(top);
+        SetBottom(bottom);
+    }
+
+    private int GetTop()
+    {
+        return _top;
+    }
+
+    private void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    private int GetBottom()
+    {
+        return _bottom;
+    }
+
+    private void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        int top = GetTop();
+        int bottom = GetBottom();
+
+        return $"{top}/{bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        int top = GetTop();
+        int bottom = GetBottom();
+
+        return (double)top / (double)bottom;
+    }
+}
+
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fractions myFraction = new Fractions();
+
+        myFraction.Fraction();
+        Console.WriteLine(myFraction.GetFractionString());
+        Console.WriteLine(myFraction.GetDecimalValue());
+        myFraction.Fraction(5);
+        Console.WriteLine(myFraction.GetFractionString());
+        Console.WriteLine(myFraction.GetDecimalValue());
+        myFraction.Fraction(3, 4);
+        Console.WriteLine(myFraction.GetFractionString());
+        Console.WriteLine(myFraction.GetDecimalValue());
+        myFraction.Fraction(1, 3);
+        Console.WriteLine(myFraction.GetFractionString());
+        Console.WriteLine(myFraction.GetDecimalValue());
     }
 }
